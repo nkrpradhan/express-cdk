85c740a3a180c36721b9e2e72c190ee296af3d0d
ankurprad <ankur.pradhan@selfridges.co.uk>
2023-12-23 11:41:16 +0000

    Initial commit

---
+++ test/fe-next-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FeNextCdk from '../lib/fe-next-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fe-next-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FeNextCdk.FeNextCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
